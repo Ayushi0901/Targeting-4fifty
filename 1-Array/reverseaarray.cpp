efe5eca473e1ff892d108ea0b83bded6cdf3fb40
Ayushi Srivastava <70138703+Ayushi0901@users.noreply.github.com>
2022-07-09 01:47:25 +0530

    updated

--- 1-Array/reverseaarray.cpp
+++ 1-Array/reverseaarray.cpp
@@ -1,27 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
-    // int n;
-    // cin>>n;
-    // for (int i = 0; i < n; ++i)
-    // {
-    //     cin>>a[i];
-    // }
-    // int j=n-1;
-    // i=0;
-    // while (i<j)
-    // {
-    //     swap(a[i],a[j])
-    // }
-    // for ( i = 0; i < n; ++i)
-    // {
-        
-    //     cout<<a[i]<<endl;
+   int n,i=0;
+cin>> n;
+int a[n];
+for(int i=0 ;i<n;++i)
+cin>>a[i];
+int j=n-1;
+i=0;
+while(i>j){
+swap( a[i],a[j]);
+    i++;
+    j--;
        
-    // }
-    // return 0;
-    
-    
-cout<<"ayushi";
-    
 }
+for(int i=0;i<n;++i)
+cout<<a[i]<<" ";
+
+return 0;
+}
+
+// Reverse the array
+// Time Complexity = O(n)
