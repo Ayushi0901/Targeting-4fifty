fee281959d42f351524453467e5e41c7aa9c57c3
Ayushi Srivastava <srivastavaayushi139@gmail.com>
2022-07-09 01:42:42 +0530

    first commit

---
+++ 1-Array/reverseaarray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cin>>n;
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     cin>>a[i];
+    // }
+    // int j=n-1;
+    // i=0;
+    // while (i<j)
+    // {
+    //     swap(a[i],a[j])
+    // }
+    // for ( i = 0; i < n; ++i)
+    // {
+        
+    //     cout<<a[i]<<endl;
+       
+    // }
+    // return 0;
+    
+    
+cout<<"ayushi";
+    
+}
